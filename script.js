acf53f89fdf9f820827157b868d18a1f5449ee23
photofaxtory <131462450+photofaxtory@users.noreply.github.com>
2024-01-27 19:13:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const smoothScroll =()=>{
+    const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+}
+
+smoothScroll ()
+
+
